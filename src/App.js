e8af1bb997628e112cd18de41e8107b04c1a2fc3
John Trujillo <amriikk@gmail.com>
2021-01-26 22:24:06 -0800

    update const variables in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tours from './components/Tours';
 import Loading from './components/Loading';
 
 const url = 'https://course.api.com/react-tours-project'
 
 function App() {
-  return  <h2>Tours Project</h2>
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  return  (
+    <main>
+      <Tours tours={tours removeTour={removeTour}} />
+    </main>
+  )
 }
 
 export default App;
